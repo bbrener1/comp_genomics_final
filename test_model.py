e2584576a51847c96d733f138ad235386640b8f8
Adhiraj <adhiraj@adhiraj-G751JY>
2017-05-04 20:33:36 -0400

    Added implementation for randomized labels

--- test_model.py
+++ test_model.py
@@ -24,20 +24,45 @@ gold = translate_gold_standard(sys.argv[1], header)
 correlation_label_net = np.load('correlation_network.npy')
 glasso_label_net = np.load('glasso_network.npy')
 
-output = open('Comparison.txt', 'w')
+output1 = open('Correlation.txt', 'w')
+output2 = open('Glasso.txt', 'w')
+output3 = open('Random_Correlation.txt', 'w')
+output4 = open('Random_Glasso.txt', 'w')
 
-output.write("For comparison using Correlation Matrix:")
+output1.write("For comparison using Correlation Matrix:")
 for i, label in enumerate(correlation_label_net):
     for j, network in enumerate(label):
-        output.write("Label " + str(i))
-        output.write("Network (threshold): " + str(j))
-        compare(network, gold, output)
+        output1.write("Label " + str(i))
+        output1.write("Network (threshold): " + str(j))
+        compare(network, gold, output1)
 
-output.write("\n\n\nFor comparison using Graphical Lasso Network:")
+output2.write("For comparison using Graphical Lasso Network:")
 for i, label in enumerate(glasso_label_net):
     for j, network in enumerate(label):
-        output.write("Label " + str(i))
-        output.write("Network (threshold): " + str(j))
-        compare(network, gold, output)
+        output2.write("Label " + str(i))
+        output2.write("Network (threshold): " + str(j))
+        compare(network, gold, output2)
 
-output.close()
+correlation_label_net = np.load('random_correlation_network.npy')
+glasso_label_net = np.load('random_glasso_network.npy')
+
+output3.write("For comparison using Correlation Matrix with random labels:")
+for i, label in enumerate(correlation_label_net):
+    for j, network in enumerate(label):
+        output3.write("Label " + str(i))
+        output3.write("Network (threshold): " + str(j))
+        compare(network, gold, output3)
+
+
+output4.write("For comparison using Graphical Lasso Network with random labels:")
+for i, label in enumerate(glasso_label_net):
+    for j, network in enumerate(label):
+        output4.write("Label " + str(i))
+        output4.write("Network (threshold): " + str(j))
+        compare(network, gold, output4)
+
+
+output1.close()
+output2.close()
+output3.close()
+output4.close()
--- train_model.py
+++ train_model.py
@@ -23,6 +23,9 @@ reduced = count_PCA(imputed)
 
 labels = GMM(reduced, "params_and_bic.txt")
 
+uniq = np.unique(np.asarray(labels))
+random_labels = rand1 = np.random.randint(len(uniq), size=(np.asarray(labels).shape[0],))
+
 # labels = GMM(reduced)
 
 # label_net = correlation_matrix(imputed, labels, "correlation_matrix.txt")
@@ -31,5 +34,11 @@ corr_label_net = correlation_matrix(imputed, labels)
 
 glasso_label_net = glasso_net(imputed, labels)
 
+random_corr_label_net = correlation_matrix(imputed, random_labels)
+
+random_glasso_label_net = glasso_net(imputed, random_labels)
+
 np.save('correlation_network.npy', corr_label_net, allow_pickle=True)
+np.save('random_correlation_network.npy', random_corr_label_net, allow_pickle=True)
+np.save('random_glasso_network.npy', random_glasso_label_net, allow_pickle=True)
 np.save('glasso_network.npy', glasso_label_net, allow_pickle=True)
